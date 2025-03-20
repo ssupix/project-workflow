7378c5f23c9d30f75a030ef99dcc640f2cdf6e2d
ssupix <151896598+ssupix@users.noreply.github.com>
2025-03-19 17:17:09 -0700

    recompiled css

--- js/theme.js
+++ js/theme.js
@@ -1,51 +1,47 @@
-// THEME SWITCH
-
-// when the document has finished loading, run the following code
 document.addEventListener('DOMContentLoaded', () => {
-    // get references to the theme toggle button and its child icons
     const themeToggle = document.getElementById('theme-toggle');
     const lightIcon = document.getElementById('light-icon');
     const darkIcon = document.getElementById('dark-icon');
-    // get a reference to the body element
     const body = document.body;
 
-    // check if a theme has been saved in localStorage
+    // Initially hide both icons
+    lightIcon.style.display = 'none';
+    darkIcon.style.display = 'none';
+
+    // Check if a theme has been saved in localStorage
     const savedTheme = localStorage.getItem('theme');
-    // if a theme has been saved, apply it to the body element
+    
+    // If a theme has been saved, apply it to the body element
     if (savedTheme) {
         body.id = savedTheme;
-        // toggle the display of the icons based on the saved theme
+        
+        // Show the correct icon based on the saved theme
         if (savedTheme === 'dark') {
-            lightIcon.style.display = 'none';
             darkIcon.style.display = 'block';
         } else {
-            darkIcon.style.display = 'none';
             lightIcon.style.display = 'block';
         }
+    } else {
+        // If no saved theme, set to light by default
+        body.id = 'light';
+        lightIcon.style.display = 'block';
     }
 
-    // an event listener to the theme toggle button
-    // when it is clicked, toggle the theme and update the icons
+    // Event listener for the theme toggle button
     themeToggle.addEventListener('click', () => {
-        // if the current theme is light, switch to dark
         if (body.id === 'light') {
             body.id = 'dark';
             lightIcon.style.display = 'none';
             darkIcon.style.display = 'block';
-            // save the new theme in localStorage
             localStorage.setItem('theme', 'dark');
-        }
-        // if the current theme is dark, switch to light
-        else if (body.id === 'dark') {
+        } else {
             body.id = 'light';
             darkIcon.style.display = 'none';
             lightIcon.style.display = 'block';
-            // save the new theme 
             localStorage.setItem('theme', 'light');
         }
     });
 
-    // update ionicons 
+    // Update ionicons
     ionicons.ionicon();
 });
-
